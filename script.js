b1206b70944ed3ddba6a92cea0517f3a5562f607
wmvieira21 <williammartinsvieira334@gmail.com>
2023-06-13 21:31:43 -0300

    as

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const mode = document.querySelector('.dark-light__mode');
+const pointerHour = document.querySelector('.pointer.hour');
 const html = document.querySelector('html');
 
 mode.addEventListener('click', (event) => {
-    console.log(html);
     html.classList.toggle('dark-mode');
+    
 });
+
+let count = 30;
+setInterval(() => {
+    pointerHour.style.transform = `translate(-50%, -50%) rotate(${360}deg)`;
+}, 1000);
